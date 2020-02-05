f62678c2a2abd06a443e4314d0038398ac914c36
dyy12358 <1829444591qq.com>
2020-02-05 13:09:10 +0800

    创建

--- src/main/java/com/example/demo/bean/config.java
+++ src/main/java/com/example/demo/bean/config.java
@@ -1,4 +1,15 @@
 package com.example.demo.bean;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class config {
+    private String Driver;
+    private String URL;
+    private String Database;
+    private String UserName;
+    private String Password;
+    private String Topic;
 }
---
+++ src/main/java/com/example/demo/util/MysqlUtil.java
@@ -0,0 +1,13 @@
+package com.example.demo.util;
+
+import com.example.demo.bean.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MysqlUtil {
+    @Autowired
+    private config config;
+
+
+}
